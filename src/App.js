912be42b92c1574f4375bf166ed5bbdafb92e0bd
kimyonghee1216 <kimyh1216@kakao.com>
2021-12-27 21:25:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './Header';
+import DayList from './DayList';
+import Day from './Day';
+import EmptyPage from './EmptyPage';
+import CreateWord from './CreateWord'
+import CreateDay from './CreateDay';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<DayList />} />
+        <Route path="day/:day" element={<Day />} />
+        <Route path="/create_word" element={<CreateWord />} />
+        <Route path="/create_day" element={<CreateDay />} />
+        <Route path="*" element={<EmptyPage />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Day.js
@@ -0,0 +1,28 @@
+//import dummy from './db/data.json'
+//import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Word from './Word'
+import useFetch from './hooks/useFetch'
+
+export default function Day(){
+  //dummy.words
+  //const day = 1
+  //const [words, setWords] = useState([])
+  const {day} = useParams()
+  //const wordList = dummy.words.filter(word =>word.day === Number(day))
+  const words = useFetch(`http://localhost:3001/words?day=${day}`)
+  //console.log(wordList)
+  return(
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {words.map(word =>(
+            <Word word={word} key={word.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+
+}
